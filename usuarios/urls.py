aeb1cd8c40acf8d719d8f1ba7d6329ada1648112
Guilherme Montosa <guilherme.montosa@hotmail.com>
2017-03-19 22:25:33 -0300

    Ajustes no url

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,9 +1,7 @@
-from django.conf.urls import include, url
-from django.contrib import admin
+from django.conf.urls import include, url, patterns
 from usuarios import views
-from django.conf import settings
-from django.conf.urls.static import static
 
-urlpatterns = [
-    url(r'^$', views.login, name='login')
-]
+urlpatterns = patterns ('',
+    url(r'^login/$', views.login, name='login'),
+    url(r'^registrar/$', views.registrar, name='registrar')
+)
